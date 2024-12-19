b99316c6e283e3bcbef5cc3bd80903783bed780e
Adrián Rodríguez Atienza <aramopelu@hotmail.com>
2024-12-19 10:40:40 +0100

    Add skelleton for frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+
+
+    return(
+    <div>
+        filter bar
+        <div>
+            Charts
+        </div>
+        <div>
+            Searchable lists
+        </div>
+    </div>
+    )
+}
+
+export default Home
