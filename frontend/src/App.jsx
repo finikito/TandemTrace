83675dfb5734c108e614ce7ff671a9c2ac05849c
Adrián Rodríguez Atienza <aramopelu@hotmail.com>
2024-12-19 10:56:16 +0100

    Add aliases for imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { DataProvider } from './context/DataContext';
+import { DataProvider } from '@context/DataContext';
 import Home from './pages/Home'
 
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@context':'/src/context',
+      '@services': 'src/services',
+      '@pages':'src/pages'
+    }
+  },
   server: {
     port: 3000,
     proxy: {
